b4c03d092d26e36c05b2550a4e3116b8b42bcc3d
Johnata Matheus <matheusabdala12@gmail.com>
2023-10-19 12:59:14 -0300

    initial commit

---
+++ src/test/java/br/com/colab/ColabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.colab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
