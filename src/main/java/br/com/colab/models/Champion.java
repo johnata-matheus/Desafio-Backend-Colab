a1f311b104d44258c975f8c6414a49119165cbad
Johnata Matheus <matheusabdala12@gmail.com>
2023-11-01 00:53:39 -0300

    feat: create champion entity relationship

--- src/main/java/br/com/colab/models/Champion.java
+++ src/main/java/br/com/colab/models/Champion.java
@@ -1,28 +1,41 @@
 package br.com.colab.models;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "tb_champion")
 public class Champion {
 
   @Id
-  @GeneratedValue(generator = "UUID")
-  private UUID id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String name;
   private String description;
   private String class_champion;
-  
   @CreationTimestamp
   private LocalDateTime createdAt;
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  @OneToMany(mappedBy = "champion")
+  private List<Skill> skills;
+
 }
